76175638183743a763ed3ff960df3c9098d56ac6
MubashirKhanYousufZai <161746933+MubashirKhanYousufZai@users.noreply.github.com>
2024-04-21 04:50:22 +0500

    today I learn array with a familier example in this files I code that example which helps me to understand it

---
+++ main.ts
@@ -0,0 +1,17 @@
+import inquirer from "inquirer"
+// array:
+let fruitChaat= ["apple","banana","orange"]
+
+console.log(fruitChaat);
+
+fruitChaat.push("melon");
+
+console.log(fruitChaat);
+
+fruitChaat.pop();
+
+console.log(fruitChaat);
+
+fruitChaat.concat(['kiwi','grapes','strawberry']);
+
+console.log(fruitChaat);
